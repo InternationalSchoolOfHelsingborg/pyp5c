273cbee90cf7c5d16187707be05f65da5d5f2eb4
InternationalSchoolOfHelsingborg <54321580+InternationalSchoolOfHelsingborg@users.noreply.github.com>
2019-08-20 19:50:39 +0200

    files

---
+++ index.js
@@ -0,0 +1,225 @@
+function validate() {
+    var name = document.forms["validation"]["name"].value;
+    var classKey = document.forms["validation"]["classkey"].value;
+    var rank = false;
+
+    if(classKey == 'Archisman053#') {
+        if(name == 'Archisman') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Alexander053#') {
+        if(name == 'Alexander') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Anusha053#') {
+        if(name == 'Anusha') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Aryan053#') {
+        if(name == 'Aryan') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Caspian053#') {
+        if(name == 'Caspian') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Zeineldin053#') {
+        if(name == 'Zeineldin') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Nevin053#') {
+        if(name == 'Nevin') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Vittoria053#') {
+        if(name == 'Vittoria') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Yaretzy053#') {
+        if(name == 'Yaretzy') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Onzo053#') {
+        if(name == 'Onzo') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Ananya053#') {
+        if(name == 'Ananya') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Elze053#') {
+        if(name == 'Elze') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Srishti053#') {
+        if(name == 'Srishti') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Saadhana053#') {
+        if(name == 'Saadhana') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Jacob053#') {
+        if(name == 'Jacob') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Maya053#') {
+        if(name == 'Maya') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == '#@$!AdMiN!$@#') {
+        if(name == 'Admin') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Yvette053#') {
+        if(name == 'Yvette') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Charvi053#') {
+        if(name == 'Charvi') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Sakina053#') {
+        if(name == 'Sakina') {
+            alert('Verified credentials. Redirecting...');
+            setTimeout(() => {
+                window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+                var nameText = document.getElementById('name');
+
+                nameText.innerHTML = name;
+            }, 2000);
+        }
+    } else if(classKey == 'Alt3053#') {
+        alert('Verified credentials. Redirecting...');
+        setTimeout(() => {
+            window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+            var nameText = document.getElementById('name');
+
+            nameText.innerHTML = name;
+        }, 2000);
+    } else if(classKey == 'Alt4053#') {
+        alert('Verified credentials. Redirecting...');
+        setTimeout(() => {
+            window.location = "https://internationalschoolofhelsingborg.github.io/pyp5c/dashboard.html";
+            var nameText = document.getElementById('name');
+
+            nameText.innerHTML = name;
+        }, 2000);
+    } else {
+        alert('Error, your class key is incorrect.');
+    }
+}
