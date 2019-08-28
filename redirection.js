b8672300566544d23efb3eac4cd57b36a5fb1828
InternationalSchoolOfHelsingborg <54321580+InternationalSchoolOfHelsingborg@users.noreply.github.com>
2019-08-28 17:39:30 +0200

    Add files via upload

---
+++ redirection.js
@@ -0,0 +1,14 @@
+var url = 'https://ishhelsingborg.goodbarber.com/pyp5-newsletters/c/0';
+var url2 = 'music.html';
+
+function loadMusic() {
+    window.location.replace(url2);
+}
+
+function loadNewsletter() {
+    window.location.replace(url);
+}
+
+function loadSchedule() {
+    window.location.replace('schema.pdf');
+}
